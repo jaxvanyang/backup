9b232413c4ec731865cb91d0997566a53aabf7af
Jax Young <jaxvanyang@gmail.com>
2021-11-16 00:11:51 +0800

    Update install_ohmyzsh_plugins.sh
    fix wrong cd directory

--- termux/install_ohmyzsh_plugins.sh
+++ termux/install_ohmyzsh_plugins.sh
@@ -17,7 +17,7 @@ done
 
 install_from_repo() {
 	echo "Installing plugin $1..."
-	git clone $2 ~/repos/$1 && cd ~/$1 && $3 && echo "Plugin $1 installed successfully" || echo "Plugin $1 installation failed"
+	git clone $2 ~/repos/$1 && cd ~/repos/$1 && $3 && echo "Plugin $1 installed successfully" || echo "Plugin $1 installation failed"
 }
 
 install_from_repo "autojump" "https://github.com/wting/autojump.git" "./install.py"
